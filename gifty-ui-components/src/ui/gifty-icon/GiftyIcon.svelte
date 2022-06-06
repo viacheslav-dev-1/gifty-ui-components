cedbdcbebff90ed53074f157514bffc8fed0d66a
Viacheslav <ekvaserega@gmail.com>
2022-06-07 02:16:23 +0300

    Added icon manager

---
+++ gifty-ui-components/src/ui/gifty-icon/GiftyIcon.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { icon, iconsLoaded } from "./icon-manager";
+
+    // Props
+    export let name;
+
+    // Component ref
+    export let ref = 'gifty-icon';
+    
+</script>
+
+{#if $iconsLoaded}
+<span 
+{ref}>
+    {@html icon(name).outerHTML}
+</span>
+{/if}
---
+++ gifty-ui-components/src/ui/gifty-icon/icon-manager.js
@@ -0,0 +1,46 @@
+import { writable } from 'svelte/store';
+
+/**
+ * Load Icons
+ * @param { string } url Request Url 
+ * @param { * } options Request Options
+ * @returns { Promise<{ success: boolean, error: string | undefined }> } Result object
+ */
+export const loadIcons = async (url, options) => {
+    try {
+        const response = await fetch(url, options);
+
+        if (response.status !== 200) {
+            return { success: false, error: 'Failed to load icons! Status code: ' + response.status };
+        }
+
+        const responseText = await response.text();
+        iconsXML = new window.DOMParser().parseFromString(responseText, 'text/xml');
+        iconsLoaded.update(() => true);
+        
+        return { success: true, error: null };
+    } catch (error) {
+        return { success: false, error };
+    }
+}
+
+/**
+ * Gets Icons XML
+ * @returns { Document } Icons XML
+ */
+export const icons = () => {
+    return iconsXML;
+}
+
+/**
+ * Gets Icon by id
+ * @param { string } id 
+ * @returns { HTMLElement } Icon html element
+ */
+export const icon = (id) => {
+    return iconsXML.getElementById(id)
+}
+
+let iconsXML = undefined;
+
+export const iconsLoaded = writable(false);
