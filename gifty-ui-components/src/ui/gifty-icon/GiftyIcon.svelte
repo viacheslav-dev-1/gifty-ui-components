f08c5ccd7757a603f526ca7f330da220c69328b4
Viacheslav <ekvaserega@gmail.com>
2022-06-07 12:55:54 +0300

    Some fixes for icon component

--- gifty-ui-components/src/ui/gifty-icon/GiftyIcon.svelte
+++ gifty-ui-components/src/ui/gifty-icon/GiftyIcon.svelte
@@ -1,17 +1,33 @@
 <script>
     import { icon, iconsLoaded } from "./icon-manager";
+    import { onMount } from 'svelte';
+    import { get } from 'svelte/store';
 
     // Props
     export let name;
-    
+
     let loaded = false;
     let iconHtml = null;
-    iconsLoaded.subscribe(it => {
-        if (it) {
-            loaded = it;
-            iconHtml = icon(name)?.outerHTML;
+    
+    onMount(() => {
+        loaded = get(iconsLoaded);
+        
+        if (!loaded) {
+            iconsLoaded.subscribe(it => {
+                if (it) {
+                    loaded = it;
+                    getIconHtml();
+                }
+            });
+        } else {
+            getIconHtml();
         }
     });
+
+    const getIconHtml = () => {
+        iconHtml = icon(name)?.outerHTML;
+    }
+    
 </script>
 
 {#if loaded}
