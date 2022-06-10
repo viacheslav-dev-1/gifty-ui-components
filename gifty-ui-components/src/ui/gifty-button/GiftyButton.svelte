2be0cb640916aeadaf7981270ed83f369181469d
Viacheslav <ekvaserega@gmail.com>
2022-06-11 01:42:57 +0300

    updated button

--- gifty-ui-components/src/ui/gifty-button/GiftyButton.svelte
+++ gifty-ui-components/src/ui/gifty-button/GiftyButton.svelte
@@ -1,23 +1,46 @@
 <script>
     import { createEventDispatcher } from 'svelte';
 
-    // Props
-    export let showIcon = true;
-    export let showText = true;
+    /**
+     * Type of button. Could be: "icon-and-text", "icon", "text"
+     */
+    export let type = 'icon-and-text'
+    /**
+     * Determine if button is rounded or not
+    */
     export let rounded = false;
+    /**
+     * Determine if button with shadow or not
+    */
     export let withShadow = false;
+    /**
+     * Determine if button is selectable or not
+     */
     export let selectable = false;
+    /**
+     * Determine if button is selected or not
+     */
     export let selected = false;
+    /**
+     * Determine if button is disabled or not
+     */
     export let disabled = false;
+    /**
+     * Determine if animate button on hover (scale up) or not
+     */
     export let animateOnHover = false;
+    /**
+     * Determine if button is without background
+     */
+    export let withoutBackground = false;
 
-    // Component ref
+    /**
+     * Gifty Button component ref
+     */
     export let ref = 'gifty-button';
 
-    // Private members
     let isSelected = selected;
-
-    // Events
+    
     const dispatch = createEventDispatcher();
 
 	const click = () => {
@@ -96,6 +119,18 @@
             transform: scale(var(--scale-size));
             transition: var(--animation-time);
         }
+
+        &.without-background {
+            background: transparent;
+
+            &.selected {
+                border: transparent 2px solid;
+            }
+        }
+
+        &.without-background:active {
+            background: transparent;
+        }
     }
 
     button:active {
@@ -113,7 +148,7 @@
     :global(.gifty-button.selected svg path)  {
         fill: var(--main-accent);
     }
-
+    
 </style>
 
 <button
@@ -124,11 +159,12 @@
     class:rounded 
     class:with-shadow="{withShadow}"
     class:disabled
+    class:without-background="{withoutBackground}"
     disabled="{disabled}"
     on:click="{click}"
     >
 
-    {#if showIcon}
+    {#if type === 'icon-and-text' || type === 'icon'}
         <slot name="icon">
             <svg id="default-gifty" viewBox="0 0 27 33" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M2.08731 13.9719L0.458282 15.4815L19.0915 32.7488L20.7205 31.2392L2.08731 13.9719Z"/>
@@ -138,8 +174,8 @@
         </slot>
     {/if}
 
-    {#if showText}
-        <span class="text" class:ml="{showIcon}">
+    {#if type === 'icon-and-text' || type === 'text'}
+        <span class="text" class:ml="{type === 'icon-and-text'}">
             <slot name="text">Gifty Button</slot>
         </span>
     {/if}
